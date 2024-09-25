0ff548b4c0ebe1160a57cc7186aec67345783264
Tane4ka170 <tane4ka170@gmail.com>
2024-09-25 23:20:25 +0300

    Added interactive card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,22 @@
 import { CardType } from "../setup";
+import { BackImg, FrontImg, Wrapper } from "./Card.styles";
 
 type Props = {
-    card: CardType;
-    callback:(card:CardType)
+  card: CardType;
+  callback: (card: CardType) => void;
 };
+
+const Card: React.FC<Props> = ({ card, callback }) => {
+  const handleClick = () => {
+    if (card.clickable) callback(card);
+  };
+
+  return (
+    <Wrapper onClick={handleClick}>
+      <FrontImg flipped={card.flipped} src={card.frontImage} alt="card-front" />
+      <BackImg flipped={card.flipped} src={card.backImage} alt="card-back" />
+    </Wrapper>
+  );
+};
+
+export default Card;
